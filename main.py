da49a1a08110a325c40c35d3dc33eace1e49b645
Joshua Nahum <josh@nahum.us>
2014-08-05 12:17:21 -0400

    Start of populations

--- main.py
+++ main.py
@@ -2,17 +2,44 @@
 Main file
 """
 import random
+import collections
 
 NUMBER_OF_SUBPOPULATIONS = 10
 SUBPOPULATION_SIZE = 100
-random_gen = random.Random()
 
 def mutation_generator():
     while True:
         yield random.expovariate(1)
 
-if __name__ == "__main__":
-    mut_gen = mutation_generator()
-    for i in range(10):
-        print(next(mut_gen))
+def create_initial_population():
+    metapopulation = []
+    for i in range(NUMBER_OF_SUBPOPULATIONS):
+        subpopulation()
 
+class Metapopulation(object):
+    def __init__(self, subpopulations):
+        self.subpopulations = subpopulations
+
+    def migrate(self):
+        pass
+
+class Subpopulation(object):
+    def __init__(self, ancestor, size):
+        self.organisms = collections.Counter()
+        self.organisms[ancestor] = size
+
+    def mutate(self):
+        pass
+
+    def select(self):
+        pass
+
+    def __repr__(self):
+        return repr(self.organisms.items())
+
+
+
+
+if __name__ == "__main__":
+    sub = Subpopulation(0.4, 10)
+    print(sub)
